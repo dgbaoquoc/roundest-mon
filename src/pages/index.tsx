80f1c2c9e4ff742b92d6c41cd6e3cb03e7824302
Quoc Dong <dgbaoquoc@gmail.com>
2021-12-31 14:56:44 +0700

    get random pokemon id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center pt-8">Which Pokemon is rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-32 h-32 bg-red-200 " />
+        <div className="w-32 h-32 bg-red-200">{first}</div>
         <div className="p-8">VS</div>
-        <div className="w-32 h-32 bg-red-200" />
+        <div className="w-32 h-32 bg-red-200">{second}</div>
       </div>
     </div>
   );
