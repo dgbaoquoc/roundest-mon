2230a24d2c3fb51696b08a294981d3e1ca0fde6a
Coup Oab <dgbaoquoc@gmail.com>
2022-01-04 15:05:03 +0700

    add SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,57 @@ import "../styles/global.css";
 import type { AppProps } from "next/app";
 import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "@/backend/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const description = "Pet project with NextJS, Prisma, PlanetScale, tRPC";
+  const title = "Roundest Pokémon";
+  const imageMetaURL = "https://roundest.t3.gg/spheal.png";
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href={"https://roundest.t3.gg/favicon.ico"} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageMetaURL} />
+        <meta name="twitter:image" content={imageMetaURL} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#000000" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=2"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Round - Video Calls Powered By T3 Tools"
+        />
+        <meta
+          name="application-name"
+          content="Round - Video Calls Powered By T3 Tools"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 function getBaseUrl() {
